50d631d69c7a0bc98f67e93c64c78af2f0c03ec5
GabiJLara <gabijlara@gmail.com>
2025-09-03 02:02:47 -0300

    Create Figura.java

---
+++ 2.º año/Objetos 1/Actividad 5/Figura.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.info.oo1.Ejercicio5_FigurasYCuerpos;
+
+public abstract class Figura extends Object {
+	
+	public abstract double getArea();
+	public abstract double getPerimetro();
+}
